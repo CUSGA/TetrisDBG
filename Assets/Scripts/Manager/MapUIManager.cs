d991abf84e4c493695992c663b860017e369f5f2
Dawncxzz <55193246+Dawncxzz@users.noreply.github.com>
2021-04-29 16:49:50 +0800

    Update MapUIManager.cs

--- Assets/Scripts/Manager/MapUIManager.cs
+++ Assets/Scripts/Manager/MapUIManager.cs
@@ -1,37 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class MapUIManager : Singleton<MapUIManager>
 {
-    public GameObject deckExplorer;
-    public GameObject deskTop;
-
-    private float count;
-    private float eachHeight;
-
-    public void Start()
+    public GameObject shopButton;
+    public int curLevel;
+    public new void Awake()
     {
-        count = deskTop.GetComponent<GridLayoutGroup>().constraintCount;
-        eachHeight = deskTop.GetComponent<GridLayoutGroup>().cellSize.y + deskTop.GetComponent<GridLayoutGroup>().spacing.y;
-    }
-
-    public void OpenDeckExplorer()
-    {
-        deckExplorer.SetActive(true);
-    }
-
-    public void UpdateDeskTopHeight() 
-    {
-        float childHeight;
-        childHeight = eachHeight * Mathf.Ceil((float)(deskTop.transform.childCount / count));
-        if(deskTop.transform.childCount > 2)
-            Debug.Log(deskTop.transform.childCount);
-        float parHeight;
-        parHeight = deskTop.GetComponent<RectTransform>().rect.height;
-        float newHeight;
-        newHeight = parHeight > childHeight ? parHeight : childHeight;
-        deskTop.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, newHeight);
+        base.Awake();
+        DontDestroyOnLoad(this);
+        if ((curLevel + 1) % 3 == 0)
+        {
+            shopButton.SetActive(true);
+        }
+        else
+        {
+            shopButton.SetActive(false);
+        }
     }
 }
